5b133321238742184dc0abd763496f32c936d9de
fidelsanchezbravo3 <fidelsanchezbravo3@github.com>
2023-01-05 19:07:00 -0600

    Fin de la seccion 2

--- 01-initial-demo/components/Navbar.jsx
+++ 01-initial-demo/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import Link from 'next/link';
 import { ActiveLink } from './ActiveLink';
 import styles from './Navbar.module.css'
+import { menuItems } from '../helpers/getNavItems';
 
 export const Navbar = () => {
   return (
     <nav className={styles['menu-container']}>
-        <ActiveLink text="Home" href="/" />
-        <ActiveLink text="About" href="/about" />
-        <ActiveLink text="Contact" href="/contact" />
+      {
+        menuItems.map(({ text, href }) => (
+          <ActiveLink key={href} text={text} href={href} />
+        ))
+      }
     </nav>
   );
 }
---
+++ 01-initial-demo/helpers/getNavItems.js
@@ -0,0 +1,19 @@
+export const menuItems = [
+    {
+        text: 'Home',
+        href: '/'
+    },
+    {
+        text: 'About',
+        href: '/about'
+    },
+    {
+        text: 'Contact',
+        href: '/contact'
+    },
+    {
+        text: 'Pricing',
+        href: '/pricing'
+    },
+
+];
