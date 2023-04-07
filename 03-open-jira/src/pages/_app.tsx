92d1c3c3b5c198d40a17909a9683a3522484dfcc
Fidel Adrian Sanchez Bravo <adrianbra07@gmail.com>
2023-04-07 16:11:54 -0600

    Inicio seccion 7

---
+++ 03-open-jira/src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import type { AppProps } from 'next/app'
+import { lightTheme, darkTheme } from '../themes';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={ darkTheme }>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
---
+++ 03-open-jira/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+import { Layout } from '../components/layouts';
+
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <Typography variant='h1' color='primary'>Hola Mundo</Typography>
+    </Layout>
+  )
+}
